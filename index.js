9c52bf63252cb4e1ba8f2e84261d884c1fc7c4a8
Detricklew <Detricklew@gmail.com>
2022-12-25 15:02:11 -0600

    updates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var log = require("./Load_discord.js");
 const { token } = require("./config.json");
-const { Load_DB } = require("./Load_discord.js");
+const { Load_DB, Log_message, Load_Server } = require("./Load_discord.js");
 // Require the necessary discord.js classes
 const {
   Client,
@@ -9,7 +9,7 @@ const {
   SlashCommandStringOption,
   Guild,
 } = require("discord.js");
-Load_DB(); 
+Load_DB();
 // Create a new client instance
 const client = new Client({
   intents: [
@@ -26,10 +26,16 @@ const client = new Client({
 client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
   c.guilds.cache.forEach((server) => {
+    Load_Server(server);
     server.channels.fetch().then((channel) => {
-      channel.forEach((billy) => {
-        if (billy.type == "0") {
-          log.Load_Messages(billy);
+      channel.forEach((channel) => {
+        if (channel.type == "0") {
+          channel.messages.fetch({ limit: 1 }).then((message) => {
+            message.forEach((message1) => {
+              Log_message(message1);
+            });
+          });
+          log.Load_Messages(channel);
         }
       });
     });
@@ -51,12 +57,12 @@ client.once(Events.ClientReady, (c) => {
 });
 
 client.on("messageCreate", async (message) => {
-        log.Log_message(message);
+  log.Log_message(message);
 });
 
 client.on("messageReactionAdd", async (MessageReaction, user) => {
-  console.log(user);
-  console.log(MessageReaction);
+  console.log(user.id);
+  console.log(MessageReaction.emoji.name);
 });
 
 // Log in to Discord with your client's token
